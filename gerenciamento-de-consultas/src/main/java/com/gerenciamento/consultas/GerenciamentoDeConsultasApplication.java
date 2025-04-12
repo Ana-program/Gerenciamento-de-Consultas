199dd4377d2aa02e0bd91926eec41e969f159b23
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2025-04-12 01:55:55 -0300

    feat: Criar GerenciamentoDeConsultasApplication

---
+++ gerenciamento-de-consultas/src/main/java/com/gerenciamento/consultas/GerenciamentoDeConsultasApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciamento.consultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeConsultasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeConsultasApplication.class, args);
+	}
+
+}
